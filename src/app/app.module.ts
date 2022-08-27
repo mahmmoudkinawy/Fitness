443e83975f403d41300f2410a37700b1e35a5fb7
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-08-27 15:39:50 +0200

    Added all the needed components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CurrentTrainingComponent } from './pages/training/current-training/current-training.component';
+import { NewTrainingComponent } from './pages/training/new-training/new-training.component';
+import { PastTrainingComponent } from './pages/training/past-training/past-training.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { RegisterComponent } from './pages/authentication/register/register.component';
+import { LoginComponent } from './pages/authentication/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrentTrainingComponent,
+    NewTrainingComponent,
+    PastTrainingComponent,
+    WelcomeComponent,
+    RegisterComponent,
+    LoginComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
