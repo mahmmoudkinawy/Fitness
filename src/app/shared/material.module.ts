67845cbaa9975c2acff2e78f4f92db83fc8702b8
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-08-30 21:07:43 +0200

    added toolbar and sidebar

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -6,6 +6,8 @@ import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const MaterialComponents = [
   MatButtonModule,
@@ -14,6 +16,8 @@ const MaterialComponents = [
   MatDatepickerModule,
   MatNativeDateModule,
   MatCheckboxModule,
+  MatSidenavModule,
+  MatToolbarModule,
 ];
 
 @NgModule({
